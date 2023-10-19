8571ab2d4c8676b41ba3c101e317d0b4d74f79b8
mochazzi <79453674+mochazzi@users.noreply.github.com>
2023-10-20 02:25:27 +0800

    Interaction between plier and cable

---
+++ Assets/SliceObject.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EzySlice;
+using UnityEngine.InputSystem;
+
+public class SliceObject : MonoBehaviour
+{
+    public Transform startSlice;
+    public Transform endSlice;
+    public LayerMask sliceableLayer;
+    public VelocityEstimator velocityEstimator;
+    public GameObject target;
+    public Material crossSectionMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        bool hasHit = Physics.Linecast(startSlice.position, endSlice.position, out RaycastHit hit, sliceableLayer);
+        if(hasHit)
+        {
+            GameObject target = hit.transform.gameObject;
+            Slice(target);
+        }
+    }
+
+    public void Slice(GameObject target)
+    {
+        Vector3 velocity = velocityEstimator.GetVelocityEstimate();
+        Vector3 planeNormal = Vector3.Cross(endSlice.position - startSlice.position, velocity);
+        planeNormal.Normalize();
+
+        SlicedHull hull = target.Slice(endSlice.position, planeNormal);
+
+        if (hull != null)
+        {
+            GameObject upperHull = hull.CreateUpperHull(target, crossSectionMaterial);
+            SetupSlicedComponent(upperHull);
+
+            GameObject lowerHull = hull.CreateLowerHull(target);
+            SetupSlicedComponent(lowerHull);
+
+            //Destroy(target);
+        }
+    }
+
+    public void SetupSlicedComponent(GameObject slicedObject)
+    {
+        Rigidbody rb = slicedObject.AddComponent<Rigidbody>();
+        MeshCollider collider = slicedObject.AddComponent<MeshCollider>();
+        collider.convex = true;
+    }
+}
